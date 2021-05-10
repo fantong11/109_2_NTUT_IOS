4046d9ad116681307e4cea9c3815304f8d8e2002
Fantong <fankaihsiang11@gmail.com>
2021-05-10 14:46:11 +0800

    Add files via upload

---
+++ DiceGame/DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by 范桶 on 2021/5/10.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
