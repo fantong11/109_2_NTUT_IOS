b454c2719840006309f37c9cebc555e354bd88c1
Fantong <fankaihsiang11@gmail.com>
2021-05-10 14:47:01 +0800

    Add files via upload

---
+++ swiftUIPractice/swiftUIPractice/swiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticeApp.swift
+//  swiftUIPractice
+//
+//  Created by 范桶 on 2021/4/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
